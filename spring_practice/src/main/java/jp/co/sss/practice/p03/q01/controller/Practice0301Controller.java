dac817b47a70059b0958951861666e33aade44c3
knaganu <knaganu@C010423431.zipangu.cac.co.jp>
2026-05-28 17:21:24 +0900

    5/28 Practice0302Controllerまで

--- spring_practice/src/main/java/jp/co/sss/practice/p03/q01/controller/Practice0301Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p03/q01/controller/Practice0301Controller.java
@@ -1,5 +1,25 @@
 package jp.co.sss.practice.p03.q01.controller;
 
-public class Practice0301Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class Practice0301Controller {
+	@RequestMapping(path = "/input", method = RequestMethod.GET)
+	public String input() {
+		return "practice03/01/input_form";
+	}
+
+	@RequestMapping(path = "/send/get_method", method = RequestMethod.GET)
+	public String sendGet(String color) {
+		System.out.println("get" + color);
+		return "practice03/01/get_result";
+	}
+
+	@RequestMapping(path = "/send/post_method", method = RequestMethod.POST)
+	public String sendPost(String color) {
+		System.out.println("post" + color);
+		return "practice03/01/post_result";
+	}
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p03/q02/form/FavoriteForm.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p03/q02/form/FavoriteForm.java
@@ -1,5 +1,41 @@
 package jp.co.sss.practice.p03.q02.form;
 
 public class FavoriteForm {
+	public String fruit;
+	public String snack;
+	public String sweet;
+	public String sidedish;
+
+	public String getFruit() {
+		return fruit;
+	}
+
+	public void setFruit(String fruit) {
+		this.fruit = fruit;
+	}
+
+	public String getSnack() {
+		return snack;
+	}
+
+	public void setSnack(String snack) {
+		this.snack = snack;
+	}
+
+	public String getSweet() {
+		return sweet;
+	}
+
+	public void setSweet(String sweet) {
+		this.sweet = sweet;
+	}
+
+	public String getSidedish() {
+		return sidedish;
+	}
+
+	public void setSidedish(String sidedish) {
+		this.sidedish = sidedish;
+	}
 
 }
